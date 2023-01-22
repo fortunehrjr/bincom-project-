310a7dbdce28030bdd18ef0baf6b3ac96d37bc39
fortunehrjr <83907042+fortunehrjr@users.noreply.github.com>
2023-01-22 05:30:46 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+/*const boxes = document.querySelectorAll(".box");
+
+boxes.forEach((box) => {
+  box.onmouseover = function () {
+    box.childNodes.forEach((node) => {
+      if (node.className === "box") {
+        node.style = "scale: 1.2";
+      }
+    });
+  };
+
+  box.onmouseout = function () {
+    box.childNodes.forEach((node) => {
+      if (node.className === "box") {
+        node.style = "scale: 1";
+      }
+    });
+  };
+});
+*/
